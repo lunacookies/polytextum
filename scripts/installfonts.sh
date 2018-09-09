53a1612457bc73a79acb415a489a3db575490cd5
Aramis Razzaghipour <aramisnoah@gmail.com>
2018-09-09 12:36:42 +1000

    scripts: Add font installation script

---
+++ scripts/installfonts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+tkfontdir="$HOME/Library/Application Support/Adobe/CoreSync/plugins/livetype/.r"
+cd "$tkfontdir"
+
+targetdir="$HOME/.local/share/polytextum/fonts"
+
+if [ -e $targetdir ]; then
+  rm -r $targetdir
+fi
+mkdir -p $targetdir
+
+for file in .*.otf ; do
+  name=$(otfinfo -p $file)
+  cp "$tkfontdir/$file" "$targetdir/$name.otf"
+done
